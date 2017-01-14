c2618f7e0a6b5de816b024d47d3824a2a809f434
Stefan Krause <stefan.krause@infopark.de>
2017-01-14 16:32:14 +0100

    upgrade to rails 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery(with: :exception)
 
-  before_filter(:before_filter_set_current_user)
+  before_action(:before_action_set_current_user)
 
   private
 
@@ -11,9 +11,8 @@ class ApplicationController < ActionController::Base
   end
   helper_method(:current_user)
 
-  def before_filter_set_current_user
+  def before_action_set_current_user
     @current_user = User.from_session(session)
-    true
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.active_support.deprecation = :notify
   config.assets.compile = false
-  config.assets.digest = true
   config.assets.js_compressor = :uglifier
   config.cache_classes = true
   config.consider_all_requests_local = false
@@ -10,6 +9,12 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   config.log_formatter = ::Logger::Formatter.new
   config.log_level = :debug
-  config.log_tags = [:uuid]
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.log_tags = [ :request_id ]
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
